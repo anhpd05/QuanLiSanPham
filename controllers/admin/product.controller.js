80a625f0065c75e578d428f73ec5802f2588cc9c
Pham Duc Anh <banhbabanhmj123@gmail.com>
2024-05-29 14:30:00 +0700

    Toi ưu lai bo loc va tim kiem ơ admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,48 +1,34 @@
 // [GET] : /admin/products
 const Product = require("../../models/product.model");
+const filterStatusHelpers = require("../../helpers/filter-status");
+const searchHelpers = require("../../helpers/search");
 
 module.exports.index = async (req, res) => {
 
 
-    // 16.1.3 : tạo ra phần lọc theo trạng thái 
+// 16.1.3 : tạo ra phần lọc theo trạng thái 
     // console.log(req.query.status); // lấy ra thằng active , inactive ,..
-    const statusChange = req.query.status;
 
     let find = {
         deleted : false
     }
-    if(statusChange){
-        find.status = statusChange;
-    } // End 16.1.3 
-
-    //16.1.3 : Tối ưu lại những phần trạng thái product
-    let filterStatus = [
-        {
-            name : "Tất cả",
-            status :"",
-            class : ""
-        },
-
-        {
-            name : "Hoạt động",
-            status :"active",
-            class : ""
-        },
-
-        {
-            name : "Dừng Hoạt động",
-            status :"inactive",
-            class : ""
-        },
-    ]
-    if(statusChange){
-        const index = filterStatus.findIndex( item => item.status == statusChange)
-        filterStatus[index].class = "active"
-
-    }else{
-        const index = filterStatus.findIndex( item => item.status == "")
-        filterStatus[index].class = "active"
+    if(req.query.status){
+        find.status = req.query.status;
     }
+// End 16.1.3 
+
+//16.1.3 : Tối ưu lại những phần trạng thái product khi đc chọn => xanh
+    let filterStatus = filterStatusHelpers(req.query)
+// END 16.1.3
+
+// 16.1.4 : Phần tìm kiếm 
+    const objectSearch = searchHelpers(req.query)
+    console.log(objectSearch);
+
+    if(objectSearch.regex) {
+        find.title = objectSearch.regex 
+    }
+// End 16.1.4 :
   
 
     const products = await Product.find(find)
@@ -50,6 +36,8 @@ module.exports.index = async (req, res) => {
     res.render("admin/pages/products/index.pug" , {
         pageTitle : "Trang danh sách sản phẩm ",
         products : products,
-        filterStatus : filterStatus
+        filterStatus : filterStatus,
+        keyword : objectSearch.keyword,
+
     }) 
 }
