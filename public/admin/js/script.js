aef2cf712a7154779794d7a42fffdef2a10867d3
Pham Duc Anh <banhbabanhmj123@gmail.com>
2024-06-02 16:04:43 +0700

    Thêm moi 1 san pha cung preview

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -84,3 +84,27 @@ if (buttonDeleteItem.length > 0) {
   })
 }
 // End Delete item
+
+// PreView Images
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage) {
+    const uploadImageInput =  document.querySelector("[upload-image-input]");
+    const uploadImagePreview =  document.querySelector("[upload-image-preview]");
+    uploadImageInput.addEventListener("change", e => {
+        console.log(e);
+        const file = e.target.files[0];
+        if(file) {
+            uploadImagePreview.src = URL.createObjectURL(file)
+        }
+        const imageClose = document.querySelector(".image-close")
+        console.log(imageClose)
+        imageClose.addEventListener("click" , () => {
+            uploadImagePreview.src ="";
+            uploadImageInput.value="";
+            // console.log(uploadImagePreview.src)
+
+        })
+    });
+}
+
+// End PreView Images
