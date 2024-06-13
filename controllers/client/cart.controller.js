d159e7fac978f8fbf4da0599922d49fe355c7ce6
Pham Duc Anh <banhbabanhmj123@gmail.com>
2024-06-14 01:18:30 +0700

    Trang gio hang

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../../models/product.model");
 const Cart = require("../../models/cart.model");
+const productHelpers = require("../../helpers/product");
 
 // [POST] : /cart/add/6669fb01861b8b1bf9123a73
 module.exports.addPost = async (req, res) => {
@@ -48,3 +49,34 @@ module.exports.addPost = async (req, res) => {
 
     res.redirect("back");
 }
+
+// [GET] /cart
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId;
+  
+    const cart = await Cart.findOne({
+      _id: cartId,
+    })
+  
+  
+    cart.totalPrice = 0;
+  
+    for (const item of cart.products) {
+      const product = await Product.findOne({
+        _id: item.product_id
+      }).select("title price discountPercentage thumbnail slug");
+  
+      product.newPrice = productHelpers.newPriceProduct(product);
+  
+      item.totalPrice = item.quantity * product.newPrice;
+  
+      item.productInfor = product;
+    }
+  
+    cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0);
+  
+    res.render("client/pages/cart/index", {
+      pageTitle: "Giỏ hàng",
+      cartDetail: cart,
+    });
+  }
